bd8796eea691db439d3ce66010ec5fad33037e5d
wyjeon <wyjeon.info@gmail.com>
2021-02-28 13:24:05 +0900

    리액트 프로젝트에 리덕스 적용

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
+
+// 스토어 만들기, Redux DevTool 적용
+const store = createStore(rootReducer, composeWithDevTools());
+// 프로젝트에 리덕스 적용
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
